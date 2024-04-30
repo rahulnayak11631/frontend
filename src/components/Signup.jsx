2f462332163f52d26c41b2be81b2d6513341e2c2
rahul nayak <rahul_nayak@thbs.com>
2024-04-30 16:26:44 +0530

    add otp page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -135,7 +135,7 @@ function Signup() {
             <>
               
               <div className="relative z-0 w-full mb-5 group">
-                <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="phone" value={formData.phone} onChange={handleChange} id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Phone number" required />
+                <input type="tel" pattern="[0-9]{10}" name="phone" value={formData.phone} onChange={handleChange} id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Phone number (Ex: 123-456-7890)" required />
               </div>
               <div className="relative z-0 w-full mb-5 group">
                 <input type="text" name="company" value={formData.company} onChange={handleChange} id="floating_company" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Company (Ex. Google)" required />
