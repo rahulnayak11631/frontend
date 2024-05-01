4818b9339d581755325349d5d85e21bf4a6ba6a4
Rahul Nayak <rahulnayak1702@gmail.com>
2024-05-01 23:48:40 +0530

    add fetch event provider component

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -2,14 +2,17 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import GetEvents from "../components/GetEvents.jsx";
+import GetEventProviders from "../components/GetEventProviders.jsx";
 
 function AdminDashboard() {
     const [showEvents, setShowEvents] = useState(false);
+    const [showEventProviders, setShowEventProviders] = useState(false);
 
     return (
         <>
-            <Navbar onGetEventsClick={() => setShowEvents(true)} />
+            <Navbar onGetEventsClick={() => setShowEvents(true)} onGetEventProvidersClick={() => setShowEventProviders(true)} />
             {showEvents && <GetEvents />}
+            {showEventProviders && <GetEventProviders />}
         </>
     );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 // Navbar.jsx
 import React from "react";
 
-function Navbar({ onGetEventsClick }) {
+function Navbar({ onGetEventsClick , onGetEventProvidersClick}) {
     return (
         <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -24,8 +24,8 @@ function Navbar({ onGetEventsClick }) {
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
                     <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-                        <li>
-                            <a href="/" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Home</a>
+                    <li>
+                            <button onClick={onGetEventProvidersClick} className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Get Event Providers</button>
                         </li>
                         <li>
                             <button onClick={onGetEventsClick} className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Get Events</button>
