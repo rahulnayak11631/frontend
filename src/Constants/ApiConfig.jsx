af2c7fd214e3c6a64eb96806eb7169cd7dffefa9
rahul nayak <rahul_nayak@thbs.com>
2024-05-29 14:59:11 +0530

    updated ip to localhost in apiconfig

--- src/Constants/ApiConfig.jsx
+++ src/Constants/ApiConfig.jsx
@@ -1,4 +1,4 @@
 export const apiConfig = {
-    baseURL: 'http://172.31.44.86:8090/api',
-    adminURL: 'http://172.31.44.86:8090/admin'
+    baseURL: 'http://localhost:8090/api',
+    adminURL: 'http://localhost:8090/admin'
 };
