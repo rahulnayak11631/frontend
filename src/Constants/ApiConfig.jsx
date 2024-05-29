acee5b9b62d06d797ef8b5232c44534395e883e5
rahul nayak <rahul_nayak@thbs.com>
2024-05-29 12:49:14 +0530

    updated api config to static url

--- src/Constants/ApiConfig.jsx
+++ src/Constants/ApiConfig.jsx
@@ -1,4 +1,4 @@
 export const apiConfig = {
-    baseURL: 'http://3.111.246.82:8090/api',
-    adminURL: 'http://3.111.246.82:8090/admin'
+    baseURL: 'http://172.31.44.86:8090/api',
+    adminURL: 'http://172.31.44.86:8090/admin'
 };
